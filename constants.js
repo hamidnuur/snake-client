5cc28ba16089dbeef088e02b4f78f83803d54f0e
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-12-01 23:49:58 +0000

    finally get it right the snaake is working well

---
+++ constants.js
@@ -0,0 +1,22 @@
+const IP = '10.0.2.15';
+const PORT = 50541;
+const MOVE_UP = 'w';
+const MOVE_LEFT = 'a';
+const MOVE_DOWN = 's';
+const MOVE_RIGHT = 'd';
+const NAME = 'Ahmed';
+const MESSAGES =
+  {x: 'Say: Anaconda is here!',
+    y: "Say: You Won't have chance!",
+    z: "Say: I'm going to eat you!"};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  NAME,
+  MESSAGES
+};
--- play.js
+++ play.js
@@ -5,3 +5,12 @@ const { setupInput } = require('./input');
 console.log('Connecting ...');
 
 setupInput(connect());
+
+
+/*const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};*/
